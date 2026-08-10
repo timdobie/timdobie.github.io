e3aa1f6ec9af87f138bb3451382764764a1a8503
timdobie <timdobie@hotmail.com>
2026-08-10 14:23:42 +1000

    hide nye

--- gig-data.js
+++ gig-data.js
@@ -16,20 +16,6 @@
   // Edit this list only when adding or changing gigs.
   const gigs = [
     {
-      date: "2026-12-31",
-      dayDate: "Thursday 31st December 2026",
-      shortLabel: "Thurs Dec 31st",
-      onStage: "8 pm",
-      venueName: "The Green Room Wine and Whiskey Bar",
-      venueShort: "The Green Room",
-      address: "94 Pakington St, Geelong West VIC 3218",
-      mapsQuery: "The Green Room Wine and Whiskey Bar 94 Pakington St, Geelong West VIC 3218",
-      venueUrl: "https://www.facebook.com/p/The-Green-Room-Wine-Whisky-and-Cocktail-Bar-100043234315014/",
-      notes: "Free entry / 3 sets",
-      posterThumb: "images/gigs/GreenRoom31122026sml.png",
-      posterFull: "images/gigs/GreenRoom31122026sml.png"
-    },
-    {
       date: "2026-11-27",
       dayDate: "Friday 27th November 2026",
       shortLabel: "Fri Nov 27th",
